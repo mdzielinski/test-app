8cd3a34af27a4bb7b8e7189e27d616f5ef2ae003
Mateusz Dzielinski <mdzielinski@gmail.com>
2021-11-28 20:26:32 +0100

    added additional simple tests

--- src/test/java/app/closer/testapp/flow/service/SimpleCalculatorTest.java
+++ src/test/java/app/closer/testapp/flow/service/SimpleCalculatorTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import app.closer.testapp.data.Expression;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,38 +14,58 @@ class SimpleCalculatorTest {
 
   private SimpleCalculator calculator;
 
-  static Stream<Arguments> PositiveAndNegativeValues() {
-    return Stream.of(
-        arguments("0+0", "0"),
-        arguments("-1+1", "0"),
-        arguments("1+-1", "0"),
-        arguments("1+1", "2"));
+  @Nested
+  class basic_adding {
+    @ParameterizedTest
+    @MethodSource("positiveAndNegativeValues")
+    void should_add(String expression, String result) {
+
+      assertEquals(calculator.calculate(Expression.from(expression)).toString(), result);
+    }
+
+    static Stream<Arguments> positiveAndNegativeValues() {
+      return Stream.of(
+          arguments("0+0", "0.0"),
+          arguments("-1+1", "0.0"),
+          arguments("1+-1", "0.0"),
+          arguments("-1+-1", "-2.0"));
+    }
   }
 
-  static Stream<Arguments> parenthesisUsage() {
-    return Stream.of(
-        arguments("0*0", "0"),
-        arguments("-1*1", "-1"),
-        arguments("1*-1", "-1"),
-        arguments("1+1", "2"));
+  @Nested
+  class basic_multiplication {
+    @ParameterizedTest
+    @MethodSource("basicMultiplication")
+    void should_multiply(String expression, String result) {
+
+      assertEquals(calculator.calculate(Expression.from(expression)).toString(), result);
+    }
+
+    static Stream<Arguments> basicMultiplication() {
+      return Stream.of(
+          arguments("0*0", "0.0"),
+          arguments("1*0", "0.0"),
+          arguments("-1*1", "-1.0"),
+          arguments("-1*-1", "1.0"),
+          arguments("1*-1", "-1.0"));
+    }
   }
 
-  @BeforeEach
-  void setUp() {
-    this.calculator = new SimpleCalculator();
-  }
+  @Nested
+  class basic_brackets {
+    @ParameterizedTest
+    @MethodSource("simpleBracketsUsage")
+    void should_consider_brackets(String expression, String result) {
 
-  @ParameterizedTest
-  @MethodSource("PositiveAndNegativeValues")
-  void should_add_positive_and_negative_values(String expression, String result) {
+      assertEquals(calculator.calculate(Expression.from(expression)).toString(), result);
+    }
 
-    assertEquals(calculator.calculate(Expression.from(expression)).toString(), result);
-  }
-
-  @ParameterizedTest
-  @MethodSource("PositiveAndNegativeValues")
-  void should_multiply(String expression, String result) {
-
-    assertEquals(calculator.calculate(Expression.from(expression)).toString(), result);
+    static Stream<Arguments> simpleBracketsUsage() {
+      return Stream.of(
+          arguments("(2+2)*2", "8.0"),
+          arguments("2+2*2", "6.0"),
+          arguments("5*(5*0)+1", "1.0"),
+          arguments("5+2*(3-2)", "7.0"));
+    }
   }
 }
